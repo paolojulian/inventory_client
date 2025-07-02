145235335f98f26854505a39bd334b8e20aad8b4
Paolo Vincent Julian <paolo.julian@accelo.com>
2025-07-02 21:34:03 +0800

    style: table header

--- src/components/shared/AppTable/AppTableHeader.tsx
+++ src/components/shared/AppTable/AppTableHeader.tsx
@@ -1,14 +1,19 @@
+import { AppText } from '@/components/shared/AppText';
 import cn from '@/utils/cn';
 import { type TableHTMLAttributes } from 'react';
 
 type Props = TableHTMLAttributes<HTMLTableCellElement>;
 
-const AppTableHeader = ({ className, ...props }: Props) => {
+const AppTableHeader = ({ className, children, ...props }: Props) => {
   return (
     <th
-      className={cn('border-b border-grey/10 px-4 py-2', className)}
+      className={cn('border-b border-grey/10 px-4 py-3', className)}
       {...props}
-    ></th>
+    >
+      <AppText as='span' className='font-medium'>
+        {children}
+      </AppText>
+    </th>
   );
 };
 
